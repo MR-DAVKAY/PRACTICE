647916708178e4f6505d1de7af712888979ff904
David Muvai <davidmuvai65@gmail.com>
2024-02-18 15:50:45 +0300

    commit

---
+++ index.php
@@ -0,0 +1,207 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  <title>VELELAZE SOFTWARES</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet"href="css/style.css">
+  </head>
+  <body>
+  
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">VELELAZE SOFTWARES</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+       
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Softwares
+          </a>
+          <ul class="dropdown-menu dropdown-menu-info">
+         
+            <li><a class="dropdown-item" href="#academic">Academic Writing</a></li> 
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#reading">Reading Books</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item"href="#affiliate">Affiliate Program</a></li>
+
+          </ul>
+          <li class="nav-item">
+          <a class="nav-link" href="#contact">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About</a>
+        </li>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+<div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/student_2.jpg" class="d-block w-100" alt="Academic Writing">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Academc Writing</h5>
+        <p>Did you know you can make a good income from Academic Writing?.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/book_2.jpg" class="d-block w-100" alt="Reading Books">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Reading Books</h5>
+        <p>Purchase the books here to improve your reading and speech skills.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/soft.jpg" class="d-block w-100" alt="Affiliate Program">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Affiliate Program</h5>
+        <p>Affiliate Marketing allows you to earn by inviting your friends at the comfort of your bed.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<a id="academic">
+<section class="my-4"
+<div class="py-4">
+<h2 class="text-center">Academic Writing</h2>
+</div>
+<div class="container-fluid">
+  <div class="row">
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/student_2.jpg" class="img-fluid pb-3">
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/student_4.png" class="img-fluid pb-3">
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/student_1.jpg" class="img-fluid pb-3">
+</div>
+</div>
+</div>
+</section>
+</a>
+
+
+<a id="reading">
+<section class="my-4"
+<div class="py-4">
+<h2 class="text-center">Reading Books</h2>
+</div>
+<div class="container-fluid">
+  <div class="row">
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/book_2.jpg" class="img-fluid pb-3">
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/student_2.jpg" class="img-fluid pb-3">
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/book_1.jpg" class="img-fluid pb-3">
+</div>
+</div>
+</div>
+</section>
+</a>
+
+<a id="affiliate">
+<section class="my-4"
+<div class="py-4">
+<h2 class="text-center">Affiliate Program</h2>
+</div>
+<div class="container-fluid">
+  <div class="row">
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/travel_2.jpeg" class="img-fluid pb-3">
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/student_1.jpg" class="img-fluid pb-3">
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/student_2.jpg" class="img-fluid pb-3">
+</div>
+</div>
+</div>
+</section>
+</a>
+
+<a id="contact">
+<section class="my-4">
+       <div class="py-4">
+              <h3 class="text-center">Promote Velelaze Softwares</h3>
+              
+</div>
+              <div class="w-50 m-auto"
+       <form action="about.php" method="post">
+              <div class="form-group">
+              <label>Name:</label>
+               <input type="text" name="name"class="form-control">
+</div> <br>
+<div class="form-group">
+                     <label>Amount:</label>
+                     <input type="amount" name="amount"class="form-control">
+       </div> <br>
+       <div class="form-group">
+                     <label>Phone number:</label>
+                     <input type="text" name="number"class="form-control">
+       </div> 
+       </form><br>
+       <button class="btn btn-success" type="submit">MAKE PAYMENT</button>
+
+
+      </div>
+</section>
+</a>
+
+<a id="about">
+<section class="my-4">
+       <div class="py-4">
+              <h2 class="text-center">About</h2>
+</div>
+<div class="container-fluid">
+       <h3 class="text-center">Velelaze Softwares<br></h3>
+       <p class="text-center"><b>I am a passionate Software Engineer interested in working in Architecture, Academic Writing and Affiliate Marketing.
+              I am a part-time freelancer with an experience of 15 years
+       </b></p>
+</div>
+</section>
+</a>
+
+
+
+       <script scr="https://ajax.googleapis.com/ajax/libs/jquery.min.js"></script>
+       <script scr="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
+
+
+
